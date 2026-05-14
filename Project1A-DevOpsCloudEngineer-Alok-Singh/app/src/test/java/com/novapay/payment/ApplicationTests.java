18319f4e1acbb831d6677d90c968e99965d85d00
EC2 Default User <ec2-user@ip-172-31-39-6.ec2.internal>
2026-05-14 10:16:52 +0000

    simplify test setup

---
+++ Project1A-DevOpsCloudEngineer-Alok-Singh/app/src/test/java/com/novapay/payment/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.novapay.payment;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Application test passed");
+    }
+}
